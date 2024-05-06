6260e245ab2122949dc3c9bc5d299fdbb8361412
AparnnaK1 <k.aparnna33@gmail.com>
2024-05-06 18:50:13 +0530

    New Changes

--- frontend/src/Components/featured/Featured.jsx
+++ frontend/src/Components/featured/Featured.jsx
@@ -6,9 +6,12 @@ import FeaturedCard from "./FeaturedCard"
 const Featured = () => {
   return (
     <>
-      <section className='featured background'>
+      <section className='featured background'style={{backgroundColor:'#28262627'}}>
         <div className='container'>
           <Heading title='Loan Providers' subtitle='Details Of Banks Providing Loan.' />
+          <br></br>
+          <p style={{Rightmargin:'-10px'}}>Click On The Bank Below To Apply For Loan</p>
+          <br></br>
           <FeaturedCard />
         </div>
       </section>
